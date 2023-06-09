c8cc74bd14faf3836fb2e5ab1d0e9f256ea9c484
Izuchukwu Uchendu <izuchukwu.uchendu@gmail.com>
2023-06-09 16:07:31 +0100

    Successful debugging

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -1,7 +1,18 @@
+'use client';
 import React from 'react';
+import { useState } from 'react';
+import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
+import Form from '@components/Form';
+const CreatePrompt = () => {
+	const [submitting, setsubmitting] = useState(false);
+	const [post, setPost] = useState({
+		prompt: '',
+		tag: '',
+	});
 
-const page = () => {
-	return <div>page</div>;
+	const CreatePrompt = async (e) => {};
+	return <Form />;
 };
 
-export default page;
+export default CreatePrompt;
